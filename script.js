a032ead34036d922bc0222591c464684690c5826
dorjeman <guydjohnston@gmail.com>
2024-09-30 15:41:53 +0100

    Fix formatting for mobile menu, add JS to close menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const menuBtn = document.getElementById('menu-btn');
 const navLinks = document.getElementById('nav-links');
+const navLinkItems = document.querySelectorAll('.nav-links a');
 
+// Open menu when menu button is clicked
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle('active');
+});
+
+// Close menu when one of the navigation links is clicked
+navLinkItems.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove('active');
+    })
 });
